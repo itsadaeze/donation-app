1f7c29a1ac55c9d295e164c7f8011d9aa0c9d99e
itsadaeze <adaezeugwu6@gmail.com>
2025-05-07 13:18:04 +0100

    add navigation

---
+++ screens/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
