d2e357ddcb8cfcbcc2a7d36466a31b2c3ab6f609
itsadaeze <adaezeugwu6@gmail.com>
2025-05-07 22:42:55 +0100

    added header

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -6,7 +6,7 @@ const Stack = createStackNavigator();
 
 const MainNavigation = () => {
     return(
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{header: () => null, headerShown: false} }>
             <Stack.Screen name={Routes.Home} component={Home} />
         </Stack.Navigator>
 
--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { View, Text } from 'react-native';
+import globalStyle from '../../assets/styles/globalStyle';
+import Header from '../../components/Header/Header';
+
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello World</Text>
-      </View>
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+    <Header title={'Adaeze U.'} type={1}/>
+    <Header title={'Adaeze U.'} type={2}/>
+    <Header title={'Adaeze U.'} type={3}/>
     </SafeAreaView>
   );
 };
