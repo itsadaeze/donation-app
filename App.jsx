7174437fdc8e53dc3604459f249b44bc95ffdad0
itsadaeze <adaezeugwu6@gmail.com>
2025-05-07 11:34:48 +0100

    folder setup

---
+++ App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import {
+  SafeAreaView, Text,
+} from 'react-native';
+import { getFontFamily } from './assets/fonts/helper';
+
+const App = () => {
+  return <SafeAreaView>
+ <Text style={{
+   fontSize: 70,
+   fontFamily: getFontFamily('Inter', '700'),
+  }}
+   >Hello World </Text>
+  </SafeAreaView>;
+
+};
+
+export default App;
